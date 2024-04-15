99090f0bf43c766755dfcf9a5004612357d3d907
Joel <98638141+joelsantosdev@users.noreply.github.com>
2024-04-15 15:46:27 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'examen-upax-front',
+  webDir: 'www'
+};
+
+export default config;
